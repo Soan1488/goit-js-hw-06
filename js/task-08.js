c4a751d131a5e3632ac2413c4afe1f172c53a7cf
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2022-09-14 21:00:04 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('form.login-form');
+
+form.addEventListener('submit', onFormSumbit);
+
+function onFormSumbit(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password } } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        window.alert ('ALL FIELDS MUST BE FILLED')
+    } else {
+        const formData = {
+            email: email.value,
+            password: password.value,
+        }
+        console.log(formData)
+        
+        
+    }  
+    event.currentTarget.reset();
+}
+
 
