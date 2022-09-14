e4e0fe99d923148ef4522bcdaf8352db99ec547a
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2022-09-14 23:02:55 +0200

    task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector('#controls button[data-create]');
+const destroyBtn = document.querySelector('#controls button[data-destroy]');
+const inputEl = document.querySelector('input');
+const boxesEl = document.querySelector('#boxes');
+
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  amount = inputEl.value;
+  const arr = [];
+  for (let i = 0; i < amount; i += 1) {
+    const element = `<div style=" width: ${30 + i * 10}px; height: ${30 + i * 10}px; background-color : ${getRandomHexColor()}"></div>`;
+    arr.push(element);
+  }
+  boxesEl.insertAdjacentHTML('beforeend', arr.join(''))
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = ''
+}
+
+
