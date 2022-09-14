b6e7e8b511f1d316e3677ab25f44601bfed8bde4
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2022-09-14 21:12:28 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color')
+
+buttonEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = `${getRandomHexColor()}`
+}
