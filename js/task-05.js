8b8b35f7af0cfbcb6fe72d8d67783ea9660b9ae0
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2022-09-16 16:15:48 +0200

    fixed 5

--- js/task-05.js
+++ js/task-05.js
@@ -4,4 +4,7 @@ const outputEl = document.querySelector('#name-output');
 
 inputEl.addEventListener("input", (event) => {
         outputEl.textContent = event.currentTarget.value;
+        if (event.currentTarget.value === '') {
+                outputEl.textContent = 'Anonymous!';
+        }
 })
